5d6517b123048a45d5d8874034effeb0d3d7c9a9
GiannisTzonis <tzonis@transifex.com>
2023-11-05 19:00:36 +0200

    Minor style fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,5 @@
+// import logoImage from "";
+
 export function Navbar() {
     console.log("Navbar rendered")
 
@@ -5,6 +7,7 @@ export function Navbar() {
         <>
             <nav className="nav">
                 <label>Ol'School Gaming</label>
+                {/* <img src={logoImage} alt="Logo" className="logo-img" /> */}
                 <div className="tabs">
                     <a href="/account">Account</a>
                     <a href="/">Todos</a>
