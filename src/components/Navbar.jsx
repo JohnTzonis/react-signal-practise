b30cf46166581ee1d29ebcec2a36837b69ffe117
GiannisTzonis <tzonis@transifex.com>
2023-11-06 14:42:12 +0200

    Minor style changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,39 @@
-// import logoImage from "";
+import React, { useState, useEffect } from 'react';
 
 export function Navbar() {
-    console.log("Navbar rendered")
+    const [playerData, setPlayerData] = useState(null);
+    const accountId = "76561198072333063";
+
+    useEffect(() => {
+        const fetchPlayerData = async () => {
+            try {
+                const response = await fetch(`https://api.opendota.com/api/players/${accountId}`);
+                if (!response.ok) {
+                    throw new Error('Failed to fetch player data');
+                }
+                const data = await response.json();
+                setPlayerData(data);
+            } catch (error) {
+                console.error('Error fetching player data:', error);
+            }
+        };
+
+        fetchPlayerData();
+    }, [accountId]);
+
+    console.log("Navbar rendered");
 
     return (
         <>
             <nav className="nav">
                 <label>Ol'School Gaming</label>
-                {/* <img src={logoImage} alt="Logo" className="logo-img" /> */}
                 <div className="tabs">
-                    <a href="/account">Account</a>
+                    {playerData && (
+                        <a href="/account">{playerData.profile}</a>
+                    )}
                     <a href="/">Todos</a>
                 </div>
             </nav>
         </>
-    )
+    );
 }
