6caafe48b9d2eb80b548429b87991ab109eb72cd
GiannisTzonis <tzonis@transifex.com>
2023-11-09 13:38:46 +0200

    finished with user's authentication functionality

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -16,12 +16,13 @@ const AuthService = {
   login: async (username, password) => {
     try {
       const response = await axios.post(`${API_URL}/login`, { username, password });
-      return response.data;
+      return response.data; // Assuming the server returns user information on successful login
     } catch (error) {
       console.error('Error logging in user:', error);
       throw error;
     }
   },
+  
 };
 
 export default AuthService;
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -4,6 +4,7 @@ import AuthService from './Auth';
 const Register = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [loggedInUser, setLoggedInUser] = useState(null);
 
   const handleRegister = async () => {
     try {
@@ -16,11 +17,56 @@ const Register = () => {
     }
   };
 
+  const handleLogin = async () => {
+    try {
+      // Ensure that 'username' and 'password' are defined
+      if (!username || !password) {
+        console.error('Username and password are required for login.');
+        return;
+      }
+  
+      // AuthService.login now returns the user information directly
+      const { username: loggedInUsername } = await AuthService.login(username, password);
+      console.log('Login successful:', loggedInUsername);
+      setLoggedInUser({ username: loggedInUsername });
+      // Handle success, redirect, etc.
+    } catch (error) {
+      console.error('Error logging in user:', error);
+      // Handle error, show an alert, etc.
+    }
+  };
+  
+
+  const handleLogout = () => {
+    // Implement logout logic (clear user information, redirect, etc.)
+    setLoggedInUser(null);
+  };
+
   return (
     <div>
-      <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
-      <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button onClick={handleRegister}>Register</button>
+      {loggedInUser ? (
+        <>
+          <p>Welcome, {loggedInUser.username || 'User'}!</p>
+          <button onClick={handleLogout}>Logout</button>
+        </>
+      ) : (
+        <>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleRegister}>Register</button>
+          <button onClick={handleLogin}>Login</button>
+        </>
+      )}
     </div>
   );
 };
