2b20906ef6c2a76b983cc73b4e2259d6e2a95b1c
GiannisTzonis <tzonis@transifex.com>
2023-11-13 14:22:42 +0200

    deployment settings

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000';
+const API_URL = 'https://olsg-be-4571e51de231.herokuapp.com/';
 
 const AuthService = {
   register: async (username, password) => {
